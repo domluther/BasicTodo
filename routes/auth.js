8e08d1a7fe7391f39f28ae79ade60fc16e250109
dom <35171942+domluther@users.noreply.github.com>
2024-08-31 21:13:57 +0100

    feat: logout button added to todo page

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,7 @@ import * as AuthMiddleware from '../middleware/auth.js';
 
 const router = express.Router();
 
-// Passport says this should be a post request
-router.get('/logout', AuthMiddleware.ensureAuth, AuthController.logout);
+router.post('/logout', AuthMiddleware.ensureAuth, AuthController.logout);
 router.post('/login', AuthMiddleware.ensureGuest, AuthController.login);
 router.post('/signup', AuthMiddleware.ensureGuest, AuthController.signup);
 
